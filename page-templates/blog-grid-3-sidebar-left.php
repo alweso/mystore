27b05a025cb444d1cf75e70f7ddfef0f9ca7607c
alweso <alweso@gmail.com>
2021-05-06 17:03:17 +0200

    WIP

---
+++ page-templates/blog-grid-3-sidebar-left.php
@@ -0,0 +1,9 @@
+<?php
+/**
+* Template Name: Blog grid 3 sidebar left
+* Template Post Type: page
+* @package WordPress
+* @subpackage personal-blog
+* @since personal-blog 1.0
+*/
+get_template_part( 'template-parts/archive-post/blog' ); ?>
